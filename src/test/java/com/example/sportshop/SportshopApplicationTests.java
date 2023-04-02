f3884466950fd9f01208089ca08c50275052cf83
Nikita <nikitalagut03@gmail.com>
2023-04-02 20:47:56 +0300

    Initial commit

---
+++ src/test/java/com/example/sportshop/SportshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
